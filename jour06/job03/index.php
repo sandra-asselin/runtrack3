33ed6fa5d4899182696b9753043b8362dd8fb38e
sandra-asselin <55872985+sandra-asselin@users.noreply.github.com>
2020-09-15 12:01:45 +0200

    Jour06

---
+++ jour06/job03/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Job03</title>
+	<link href="style.css" rel="stylesheet">
+</head>
+<body>
+</body>
+</html>
---
+++ jour06/job04/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Job04</title>
+	<link href="style.css" rel="stylesheet">
+</head>
+	<header>
+		<ul>
+			<li><a href="">Accueil</a></li>
+			<li><a href="">Présentation</a></li>
+			<li><a href="">Contact</a></li>
+		</ul>
+		<div class="menu">
+            <input class="burger" type="checkbox">
+            <nav>
+                <a href="">Accueil</a>
+				<a href="">Présentation</a>
+				<a href="">Contact</a>
+            </nav>
+        </div>
+	</header>
+<body>
+</body>
+</html>
